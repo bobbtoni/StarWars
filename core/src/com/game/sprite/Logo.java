c0e40e529fc51e01793ff7d9f9d7884aa810e302
Anton <bobb.toni.phone@gmail.com>
2019-11-11 15:00:17 +0300

    Homework №-3 LibGDX. Добавил классы с вебинара.

---
+++ core/src/com/game/sprite/Logo.java
@@ -0,0 +1,12 @@
+package com.game.sprite;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import com.game.base.Sprite;
+
+public class Logo extends Sprite {
+
+    public Logo(TextureRegion region) {
+        super(region);
+    }
+}
