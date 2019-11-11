6e2f24dec8726d1edcb4e88a37d832cb7b0f4078
Anton <bobb.toni.phone@gmail.com>
2019-11-11 15:19:28 +0300

    Homework №-3 LibGDX. Добавил единую систему координат.

--- core/src/com/game/base/BaseScreen.java
+++ core/src/com/game/base/BaseScreen.java
@@ -3,83 +3,141 @@ package com.game.base;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Matrix3;
+import com.badlogic.gdx.math.Vector2;
+import com.game.math.MatrixUtils;
+import com.game.math.Rect;
 
 public class BaseScreen implements Screen, InputProcessor {
     protected SpriteBatch batch;
-    private Texture backGround;
+    private Rect screenBounds;
+    private Rect worldBounds;
+    private Rect glBounds;
+
+    private Matrix4 worldToGl;
+    private Matrix3 screenToWorld;
+
+    private Vector2 touch;
+
+    public BaseScreen() {
+        this.screenBounds = new Rect();
+        this.worldBounds = new Rect();
+        this.glBounds = new Rect(0, 0, 1f, 1f);
+        this.worldToGl = new Matrix4();
+        this.screenToWorld = new Matrix3();
+        this.touch = new Vector2();
+    }
 
     @Override
     public void show() {
+        System.out.println("show");
         Gdx.input.setInputProcessor(this);
-        batch = new SpriteBatch();
-        backGround = new Texture("StarsImg.jpg");
+        this.batch = new SpriteBatch();
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 0.9f);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(backGround, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.end();
+
     }
 
     @Override
     public void resize(int width, int height) {
+        System.out.println("resize width = " + width + " height = " + height);
+        screenBounds.setSize(width, height);
+        screenBounds.setLeft(0);
+        screenBounds.setBottom(0);
+
+        float aspect = width / (float) height;
+        worldBounds.setHeight(1f);
+        worldBounds.setWidth(1f * aspect);
+        MatrixUtils.calcTransitionMatrix(worldToGl, worldBounds, glBounds);
+        batch.setProjectionMatrix(worldToGl);
+        MatrixUtils.calcTransitionMatrix(screenToWorld, screenBounds, worldBounds);
+        resize(worldBounds);
+    }
+
+    public void resize(Rect worldBounds) {
 
     }
 
     @Override
     public void pause() {
-
+        System.out.println("pause");
     }
 
     @Override
     public void resume() {
-
+        System.out.println("resume");
     }
 
     @Override
     public void hide() {
+        System.out.println("hide");
         dispose();
     }
 
     @Override
     public void dispose() {
+        System.out.println("dispose");
         batch.dispose();
-        backGround.dispose();
     }
 
     @Override
     public boolean keyDown(int keycode) {
+        System.out.println("keyDown keycode = " + keycode);
         return false;
     }
 
     @Override
     public boolean keyUp(int keycode) {
+        System.out.println("keyUp keycode = " + keycode);
         return false;
     }
 
     @Override
     public boolean keyTyped(char character) {
+        System.out.println("keyTyped character = " + character);
         return false;
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        System.out.println("touchDown screenX = " + screenX + " screenY = " + screenY);
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        touchDown(touch, pointer);
+        return false;
+    }
+
+    public boolean touchDown(Vector2 touch, int pointer) {
+        System.out.println("touchDown touchX = " + touch.x + " touchY = " + touch.y);
         return false;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        System.out.println("touchUp screenX = " + screenX + " screenY = " + screenY);
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        touchUp(touch, pointer);
+        return false;
+    }
+
+    public boolean touchUp(Vector2 touch, int pointer) {
+        System.out.println("touchUp touchX = " + touch.x + " touchY = " + touch.y);
         return false;
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
+        System.out.println("touchDragged screenX = " + screenX + " screenY = " + screenY);
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        touchDragged(touch, pointer);
+        return false;
+    }
+
+    public boolean touchDragged(Vector2 touch, int pointer) {
+        System.out.println("touchDragged touchX = " + touch.x + " touchY = " + touch.y);
         return false;
     }
 
@@ -90,6 +148,7 @@ public class BaseScreen implements Screen, InputProcessor {
 
     @Override
     public boolean scrolled(int amount) {
+        System.out.println("scrolled amount = " + amount);
         return false;
     }
 }
--- core/src/com/game/screens/MenuScreen.java
+++ core/src/com/game/screens/MenuScreen.java
@@ -1,103 +1,59 @@
 package com.game.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.game.base.BaseScreen;
+import com.game.math.Rect;
+import com.game.sprite.Background;
+import com.game.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
-    private Texture unit;
-    private Vector2 moveVector;
-    private Vector2 unitCoord;
-    private float unitSpeed;
+    private Texture img;
+    private Texture bg;
+    private Logo logo;
+    private Background background;
+
 
     @Override
     public void show() {
         super.show();
-        unitCoord = new Vector2(0, 0);
-        moveVector = unitCoord.cpy();
-        unitSpeed = 1.0f;
-        unit = new Texture("FlyUnit.png");
+        img = new Texture("FlyUnit.png");
+        bg = new Texture("StarsImg.jpg");
+        logo = new Logo(new TextureRegion(img));
+        logo.setHeightProportion(0.2f);
+        background = new Background(new TextureRegion(bg));
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        unitCoord.add(moveVector.cpy().sub(unitCoord).limit(unitSpeed));
-        batch.draw(unit, unitCoord.x - 64, Gdx.graphics.getHeight() - unitCoord.y - 64, 128, 128);
+        background.draw(batch);
+        logo.draw(batch);
         batch.end();
     }
 
     @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-        dispose();
-    }
-
-    @Override
     public void dispose() {
+        img.dispose();
+        bg.dispose();
         super.dispose();
-        unit.dispose();
     }
 
     @Override
-    public boolean keyUp(int keycode) {
-        System.out.println("" + keycode);
-        switch (keycode){
-            case 19: moveVector.set(moveVector.x, unitCoord.y); break;
-            case 20: moveVector.set(moveVector.x, unitCoord.y); break;
-            case 21: moveVector.set(unitCoord.x, moveVector.y); break;
-            case 22: moveVector.set(unitCoord.x, moveVector.y); break;
-            default: break;
-        }
-
-        return false;
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
     }
 
     @Override
-    public boolean keyDown(int keycode) {
-        System.out.println("" + keycode);
-        switch (keycode){
-            case 19: moveVector.set(moveVector.x, 0); break;
-            case 20: moveVector.set(moveVector.x, Gdx.graphics.getHeight()); break;
-            case 21: moveVector.set(0, moveVector.y); break;
-            case 22: moveVector.set(Gdx.graphics.getWidth(), moveVector.y); break;
-            default: break;
-        }
-
+    public boolean touchDown(Vector2 touch, int pointer) {
         return false;
     }
 
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        moveVector.set(screenX, screenY);
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(int amount) {
-        // изменение скорости передвижения
-        if (amount == -1 && unitSpeed > 1) {
-            unitSpeed -= 1;
-        }
-        else if (amount == 1 && unitSpeed < 5){
-            unitSpeed += 1;
-        }
-        return false;
-    }
 }
